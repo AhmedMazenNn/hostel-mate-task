85f3028ae6e24e4884ced39f733d91ae82bee602
Ahmed Mazen <ahmedmazenm@outlook.com>
2025-06-26 01:16:01 +0300

    updated

--- hostel-mate/src/components/LetsMake.jsx
+++ hostel-mate/src/components/LetsMake.jsx
@@ -2,37 +2,49 @@ import React from "react";
 
 const LetsMake = () => {
   return (
-    <>
-    
-  <div className="d-flex flex-wrap align-items-center rounded-5" style={{backgroundColor: "#F3F3F3",margin: "0 auto",width:"80%",gap:"400px"}}>
     <div
+      className="d-flex flex-wrap align-items-center justify-content-between rounded-5 p-4"
       style={{
-        width: "30%",
-        marginLeft:"50px"
+        backgroundColor: "#F3F3F3",
+        margin: "0 auto",
+        width: "80%",
+      }}
+    >
+      {/* Text Content on the Left */}
+      <div style={{ flex: "1 1 50%", padding: "20px" }}>
+        <h2 className="mb-3">Let’s make things happen</h2>
+        <p className="mb-4">
+          Contact us today to learn more about how our digital marketing
+          services can help your business grow and succeed online.
+        </p>
+        <button
+          className="request"
+          style={{
+            padding: "15px 25px",
+            borderRadius: "15px",
+            backgroundColor: "black",
+            color: "white",
+            border: "none",
+          }}
+        >
+          Get your free proposal
+        </button>
+      </div>
 
-        
-      }}>
-      <h2>Let’s make things happen</h2>
-      <p>
-        Contact us today to learn more about how our digital marketing services
-        can help your business grow and succeed online.
-      </p>
-      <button
-        className="request"
-        style={{
-          padding: "20px",
-          borderRadius: "15px",
-          backgroundColor: "black",
-          color: "white"
-        }}>
-        Get your free proposal
-      </button>
+      {/* Image on the Right */}
+      <div style={{ flex: "1 1 40%", padding: "20px" }}>
+        <img
+          src="/Letsmake.png"
+          alt="Let's Make"
+          style={{
+            width: "100%",
+            height: "auto",
+            borderRadius: "20px",
+            objectFit: "cover",
+          }}
+        />
+      </div>
     </div>
-    <div>
-        <img src="../../public/Letsmake.png" alt="" />
-    </div>
-    </div>
-    </>
   );
 };
 
